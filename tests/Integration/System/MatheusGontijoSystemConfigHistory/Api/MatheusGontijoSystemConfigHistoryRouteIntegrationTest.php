1634a6bec9c31d67b94d5d593d4f42f387b69129
Matheus Gontijo <matheus@matheusgontijo.com>
2022-09-19 13:52:56 -0300

    updated

--- tests/Integration/System/MatheusGontijoSystemConfigHistory/Api/MatheusGontijoSystemConfigHistoryRouteIntegrationTest.php
+++ tests/Integration/System/MatheusGontijoSystemConfigHistory/Api/MatheusGontijoSystemConfigHistoryRouteIntegrationTest.php
@@ -3,20 +3,13 @@
 namespace MatheusGontijo\SystemConfigHistory\Tests\Integration\External\Shopware\Core\System\SystemConfig;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\ForwardCompatibility\Result;
-use MatheusGontijo\SystemConfigHistory\System\MatheusGontijoSystemConfigHistory\MatheusGontijoSystemConfigHistoryCollection; // phpcs:ignore
-use MatheusGontijo\SystemConfigHistory\System\MatheusGontijoSystemConfigHistory\MatheusGontijoSystemConfigHistoryDefinition; // phpcs:ignore
-use MatheusGontijo\SystemConfigHistory\System\MatheusGontijoSystemConfigHistory\MatheusGontijoSystemConfigHistoryEntity;
-use MatheusGontijo\SystemConfigHistory\System\MatheusGontijoSystemConfigHistory\MatheusGontijoSystemConfigHistoryHydrator; // phpcs:ignore
 use MatheusGontijo\SystemConfigHistory\Tests\TestDefaults;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Test\TestCaseBase\AdminApiTestBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\AdminFunctionalTestBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\Framework\Uuid\Uuid;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class MatheusGontijoSystemConfigHistoryRouteIntegrationTest extends TestCase
 {
@@ -138,8 +131,6 @@ class MatheusGontijoSystemConfigHistoryRouteIntegrationTest extends TestCase
 
         static::assertEquals($responseContentExpected, $responseContentActual);
     }
-
-    // @TODO: CONVERT COLUMNS SEVERAL TESTS BY DATA PROVIDER.... MUCH BETTER!!!
 
     /**
      * @param array<string, mixed> $data
@@ -290,5 +281,40 @@ class MatheusGontijoSystemConfigHistoryRouteIntegrationTest extends TestCase
         $responseContentActual = json_decode($this->getBrowser()->getResponse()->getContent(), true);
 
         static::assertEquals($responseContentExpected, $responseContentActual);
+    }
+
+    public function testMatheusGontijoSystemConfigHistoryRevertConfigurationValue(): void
+    {
+        $systemConfigService = $this->getContainer()->get(SystemConfigService::class);
+        \assert($systemConfigService instanceof SystemConfigService);
+
+        $systemConfigService->set('my.systemConfig.aaaBbbCcc', 'foobar', TestDefaults::SALES_CHANNEL_ID_ENGLISH);
+
+        $connection = $this->getContainer()->get(Connection::class);
+        \assert($connection instanceof Connection);
+
+        $connection->insert('matheus_gontijo_system_config_history', [
+            'id' => Uuid::fromHexToBytes('4d2871498e7e4a4e91e13b8280b7d935'),
+            'configuration_key' => 'my.systemConfig.aaaBbbCcc',
+            'configuration_value_old' => null,
+            'configuration_value_new' => '{"_value":123456}',
+            'sales_channel_id' => Uuid::fromHexToBytes(TestDefaults::SALES_CHANNEL_ID_ENGLISH),
+            'username' => 'mgontijo',
+            'created_at' => '2022-09-15 19:03:54.162',
+            'updated_at' => null,
+        ]);
+
+        $this->getBrowser()->request(
+            'POST',
+            '/api/_action/matheus-gontijo/matheus-gontijo-system-config-history/revert-configuration-value',
+            [
+                'matheusGontijoSystemConfigHistoryId' => '4d2871498e7e4a4e91e13b8280b7d935',
+                'configurationValueType' => 'configuration_value_new',
+            ]
+        );
+
+        $valueActual = $systemConfigService->get('my.systemConfig.aaaBbbCcc', TestDefaults::SALES_CHANNEL_ID_ENGLISH);
+
+        static::assertSame(123456, $valueActual);
     }
 }
